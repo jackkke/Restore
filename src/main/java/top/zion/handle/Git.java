412e792025a00a50c65b8eb39c06d5f117f4599b
jackkke <hangao1204@hotmail.com>
2024-01-30 15:12:32 +0800

    资源库重名增强，支持自定义文件夹名称，防止重复

--- src/main/java/top/zion/handle/Git.java
+++ src/main/java/top/zion/handle/Git.java
@@ -1,12 +1,14 @@
 package top.zion.handle;
 
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.map.MapUtil;
 import cn.hutool.core.util.StrUtil;
 import top.zion.util.CommandUtil;
 import top.zion.util.ZipUtil;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -15,18 +17,20 @@ import java.util.List;
  */
 public class Git {
     public static void handle(String zipPath, Object git) {
-        List<String> gits = (List<String>) git;
-        if (CollUtil.isNotEmpty(gits)) {
-            gits.forEach(gitUrl -> {
-                try {
-                    String localDirectory = zipPath + File.separator + "git" + File.separator + StrUtil.subAfter(gitUrl, "/", true).replace(".git", "");
-                    int exitCode = CommandUtil.doCmd(String.format("git clone %s %s", gitUrl, localDirectory));
-                    System.out.printf("Git %s Clone end, exitCode is %s!%n", gitUrl, exitCode);
-                } catch (IOException | InterruptedException e) {
-                    System.err.printf("Git Clone error : %s%n", e.getMessage());
-                }
-            });
-            ZipUtil.zipPath(zipPath, zipPath + File.separator + "git", true);
+        if (git instanceof HashMap<?,?>) {
+            HashMap<String, String> gits = (HashMap<String, String>) git;
+            if (MapUtil.isNotEmpty(gits)) {
+                gits.forEach((name, gitUrl) -> {
+                    try {
+                        String localDirectory = zipPath + File.separator + "git" + File.separator + name;
+                        int exitCode = CommandUtil.doCmd(String.format("git clone %s %s", gitUrl, localDirectory));
+                        System.out.printf("Git %s Clone end, exitCode is %s!%n", gitUrl, exitCode);
+                    } catch (IOException | InterruptedException e) {
+                        System.err.printf("Git Clone error : %s%n", e.getMessage());
+                    }
+                });
+                ZipUtil.zipPath(zipPath, zipPath + File.separator + "git", true);
+            }
         }
     }
 }
